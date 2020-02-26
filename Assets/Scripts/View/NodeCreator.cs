bb9da93ebbed3af2000108c8e127d87ca9104c20
Pericles Telemachou <pericles@outlook.com.au>
2020-02-26 17:57:33 +1100

    various

--- Assets/Scripts/View/NodeCreator.cs
+++ Assets/Scripts/View/NodeCreator.cs
@@ -63,7 +63,13 @@ public class NodeCreator : MonoBehaviour
                 while (sign == 0)
                     sign = Random.Range(-1, 1);
 
-                if ((v3.x == xSign) && (Mathf.Abs(v3.y - originDist * ySign) <= 100f))
+                // ToDo: This statement doesn't do what it's supposed to (i.e. separate nodes too diagonally close to one another)
+                if ((Mathf.Abs(v3.x - (xSign * 100 * originDist)) <= 100f) && (Mathf.Abs(v3.y - (ySign * 100 * originDist)) <= 100f))
+                    xSign *= -1;
+                // ToDo: This statement won't solve issue if flipping the XSign will violate one of these other conditions, for a prior node
+                else if ((v3.x == (xSign * originDist)) && (v3.y == (ySign * originDist)))
+                    xSign *= -1;
+                else if ((v3.x == xSign) && (Mathf.Abs(v3.y - originDist * ySign) <= 100f))
                     xSign = sign;
                 else if (v3.x == ySign && (Mathf.Abs(v3.y - originDist * ySign) <= 100f))
                     ySign = sign;
