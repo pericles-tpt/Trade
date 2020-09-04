10606ecce52d6751dfebe559978221694872701c
pericles-tpt <pericles@outlook.com.au>
2020-09-04 10:01:57 +1000

    Terminal commands now move up, command history is saved

---
+++ Assets/Scripts/Core/Management/CommandManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UIElements;
+using System;
+
+namespace Assets.Scripts.Core.Management
+{
+    public class CommandManager : MonoBehaviour
+    {
+
+        Queue<string> CommandHistory;
+        const int CommandLimit = 100;
+
+        // Use this for initialization
+        void Start()
+        {
+            CommandHistory = new Queue<string>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public string[] ProcessCommand (string a)
+        {
+            AddCommandHistory(a);
+            return ExecuteCommand(a);
+        }
+
+        public string[] ExecuteCommand(string a)
+        {
+            return new string[] { "> " + a, "COMMAND ACCEPTED" };
+        }
+
+        public void AddCommandHistory (string a)
+        {
+            CommandHistory.Enqueue(a);
+            if (CommandHistory.Count > CommandLimit)
+            {
+                CommandHistory.Dequeue();
+            }
+        }
+    }
+}
